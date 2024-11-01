70d3cd3ab70779a8750cc3646e229edb4871678f
WaterIris <youiris13@gmail.com>
2024-11-01 18:13:44 +0100

    ...

--- config/default.nix
+++ config/default.nix
@@ -1,13 +1,7 @@
 {
   imports = [
-    ./set.nix
-    ./autoclose.nix
-    ./colors.nix
-    ./lualine.nix
   ];
  
-  config.opts.number = true;
-  config.opts.relativenumber = true;
   extraConfigLua = /* lua */ ''
     print("It's Nvimin Time")
   '';
