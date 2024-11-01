fdca23620ba43742ea2791bbe419064c8b2bffc5
WaterIris <youiris13@gmail.com>
2024-11-01 21:52:14 +0100

    x

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,8 +1,24 @@
 {
-	plugins.treesitter = {
-		enable = true;
-		settings={
-			highlight.enable = true;
-		};
-	};
+  plugins.treesitter = {
+    enable = true;
+    settings = {
+      ensure_installed = [
+        "c"
+        "cpp"
+        "rust"
+        "python"
+        "lua"
+        "vim"
+        "vimdoc"
+        "query"
+        "nix"
+      ];
+
+      highlight.enable = true;
+      indent.enable = true;
+    };
+
+    nixvimInjections = true;
+  };
+  plugins.treesitter-context.enable = true;
 }
