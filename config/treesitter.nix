8ed307a093de6f759ff9b47ec22d722eacdca969
WaterIris <youiris13@gmail.com>
2024-11-03 00:11:59 +0100

    repair treesitter nad nvim ver

--- config/treesitter.nix
+++ config/treesitter.nix
@@ -1,5 +1,19 @@
 {
     plugins.treesitter = {
         enable = true;
+        settings = {
+            ensure_installed = [
+                "c"
+                "cpp"
+                "rust"
+                "python"
+                "lua"
+                "vim"
+                "nix"
+            ];
+
+            highlight.enable = true;
+            indent.enable = true;
+        };
     };
 }
