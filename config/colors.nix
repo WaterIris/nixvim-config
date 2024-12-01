e7bef7a1723b0466d553e879042d3c4104fc3ed8
WaterIris <179727984+WaterIris@users.noreply.github.com>
2024-12-01 17:01:33 +0100

    new colorscheme 2

--- config/colors.nix
+++ config/colors.nix
@@ -10,10 +10,41 @@
       RainbowCyan.fg = "#56B6C2";
     };
 
-    colorschemes.rose-pine = {
+    colorschemes.catppuccin = {
       enable = true;
       settings = {
-        variant = "moon";
+        flavour = "mocha";
+        transparent_background = true;
+        show_end_of_buffer = true;
+
+        integrations = {
+          treesitter = true;
+          cmp = true;
+          gitsigns = true;
+          illuminate = {
+            enabled = true;
+            lsp = true;
+          };
+          native_lsp = {
+            enabled = true;
+            inlay_hints = {
+              background = true;
+            };
+            virtual_text = {
+              errors = ["italic"];
+              hints = ["italic"];
+              information = ["italic"];
+              warnings = ["italic"];
+              ok = ["italic"];
+            };
+            underlines = {
+              errors = ["underline"];
+              hints = ["underline"];
+              information = ["underline"];
+              warnings = ["underline"];
+            };
+          };
+        };
       };
     };
 
